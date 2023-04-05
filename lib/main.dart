ff6786095129f4b1c056ffffb29a6dcd499967dd
sihyeokpark <exon.indiv@gmail.com>
2023-04-05 16:25:08 +0900

    급식앱 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:ffi';
 import 'dart:convert';
 
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -37,7 +39,8 @@ class ListViewPage extends StatefulWidget {
 
 class ListViewPageState extends State<ListViewPage> {
   final List<String> _items = <String>[];
-  final _url = Uri.parse('https://xn--299a1v27nvthhjj.com/api/2023-03-31');
+  var _url = Uri.parse('');
+  final _itemsLength = <int>[];
 
   Map<String, dynamic> json = {};
 
@@ -48,59 +51,95 @@ class ListViewPageState extends State<ListViewPage> {
   }
 
   void callApi() async {
+    DateTime now = DateTime.now();
+    DateFormat formatter = DateFormat('yyyy-MM-dd');
+    _url = Uri.parse('https://xn--299a1v27nvthhjj.com/api/' + formatter.format(now));
     final response = await http.get(_url);
     json = jsonDecode(response.body);
+    int i = 0;
     setState(() {
       List<String> meals = ['breakfast', 'lunch', 'dinner'];
       meals.forEach((meal) {
+        _itemsLength.add(0);
         _items.add(meal);
         json['meal'][meal].split('/').forEach((menu) {
           _items.add(menu);
+          _itemsLength[i]++;
         });
+        i++;
       });
     });
+    
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(color: Theme.of(context).colorScheme.primary),
-        Scaffold(
-          backgroundColor: Colors.transparent,
+    double windowHeight = MediaQuery.of(context).size.height;
+    int length = 0;
+    int meal = 0;
+    return Scaffold(
+          backgroundColor: Colors.white,
           body: SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(outPadding),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: _items.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      if (['breakfast', 'lunch', 'dinner'].contains(_items[index])) {
-                        return Container(
-                          height: 50,
-                          child: Text(_items[index]),
-                          margin: const EdgeInsets.all(8.0),
-                        );
-                      } else {
-                        return Container(
-                          height: 50,
-                          color: Colors.lightBlueAccent,
-                          padding: const EdgeInsets.all(8.0),
-                          margin: const EdgeInsets.all(8.0),
-                          child: Text(_items[index]),
-                        );
-                      }
-                    },
-                  ),
-                ],
-              )
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: windowHeight-outPadding*4,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: _items.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (['breakfast', 'lunch', 'dinner'].contains(_items[index])) {
+                            Map<String, String> mealName = {
+                              'breakfast': '아침',
+                              'lunch': '점심',
+                              'dinner': '저녁',
+                            };
+                            length = 0;
+                            meal++;
+                            return Container(
+                              height: 20,
+                              margin: const EdgeInsets.all(8.0),
+                              child: Text(mealName[_items[index]]!, overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            );
+                          } else {
+                            length++;
+                            BorderRadius borderRadiusOption = const BorderRadius.only();
+                            if (length == 1) {
+                              borderRadiusOption = const BorderRadius.only(
+                                topRight: Radius.circular(20.0),
+                                topLeft: Radius.circular(20.0)
+                              );
+                            } else if (length == _itemsLength[meal-1]) {
+                              borderRadiusOption = const BorderRadius.only(
+                                bottomLeft: Radius.circular(20.0),
+                                bottomRight: Radius.circular(20.0)
+                              );
+                            }
+                            return Container(
+                              height: 50,
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(_items[index]),
+                              decoration: BoxDecoration(
+                                borderRadius: borderRadiusOption,
+                                color: const Color.fromRGBO(249, 249, 250, 1),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                ),
             )
           )
-        )
-      ]
-    );
+        );
   }
 }
